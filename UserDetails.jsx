b9ca4e750404c82fee002cdbc440661f037394f1
CALVINSEABALA <36844623+CALVINSEABALA@users.noreply.github.com>
2021-07-28 17:09:55 +0200

    Update UserDetails.jsx

--- UserDetails.jsx
+++ UserDetails.jsx
@@ -1,28 +1,67 @@
 import React, {Component} from "react";
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default class UserDetails extends Component {
+
     state = {
-        age : 20,
-        name : 'Calvin',
-        surname: 'Seabala',
-        location: 'Mamotintane',
+        users: [
+            {
+                id:1,
+                age : 25,
+                name : 'Calvin',
+                surname: 'Seabala',
+                location: 'Mamotintane',
+            },
+            {
+                id:2,
+                age : 27,
+                name : 'Tyson',
+                surname: 'Mothlabeng',
+                location: 'Matlala',
+            },
+            {
+                id:3,
+                age : 17,
+                name : 'Andries',
+                surname: 'Sebola',
+                location: 'Tzaneen',
+            }
+        ]
 
     };
-    
+
+
+
+
     render() {
+        const UserDetails = this.state.users.map((user,id) => {
+            return (
+                <div  key={id}>
+                    <p className={'content-item'}>
+                        {user.name}  {user.surname} {this.ConfirmAge(user.age)}
+                    </p>
+
+                </div>
+
+            )
+        });
+
         return (
-            <div>
+            <div className={'container'}>
+
             <h1> Welcome</h1>
-                <h2>{this.state.name}  {this.state.surname} </h2>
-                <h2>|{this.state.age} {this.ConfirmAge()} </h2>
+           <div className={'row'}>
+               {UserDetails}
+           </div>
             </div>
         );
     }
-    ConfirmAge (){
+    ConfirmAge = (age) => {
         let message ='';
 
-        if (this.state.age > 18){
+        if (age > 18){
              message =  'you are old enough'
         }
         else{
